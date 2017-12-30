cb8508d454ea3a29dc5708ebcff303ed140a3116
TseJunZhen <tsejunzhen95@hotmail.com>
2017-12-30 23:21:39 +0800

    Add files via upload

--- Assets/Script/scrollImage.cs
+++ Assets/Script/scrollImage.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class scrollImage : MonoBehaviour {
-	float scrollSpeed = -2f;
+	float scrollSpeed;
 	Vector2 startPos;
 	// Use this for initialization
 	void Start () {
+		scrollSpeed = -0.2f;
 		startPos = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		scrollSpeed -= Time.deltaTime*0.1;
 		float newPos = Mathf.Repeat(Time.time * scrollSpeed,50);
 		transform.position = startPos + Vector2.down * newPos;
 	}
