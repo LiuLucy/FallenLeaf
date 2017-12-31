9af430257c5621aaa3765aabe44bfb3099cad6e0
TseJunZhen <tsejunzhen95@hotmail.com>
2017-12-31 11:39:32 +0800

    record

--- Assets/Script/AddCoin.cs
+++ Assets/Script/AddCoin.cs
@@ -3,29 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AddCoin : MonoBehaviour {
-	// Use this for initialization
-	public Text score_Coin;
-	private int score_Count = 0;
-	private GameObject coin;
-	void Start () {
-		score_Coin.text = score_Count.ToString();
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		coin = GameObject.FindGameObjectWithTag("coin");
-		Destroy(coin,5);
-	}
-	 void OnTriggerEnter2D(Collider2D other) {
-		 Debug.Log("123");
-        if(other.gameObject.tag == "coin")
-		{
-			score_Count+=1;
-			score_Coin.text = score_Count.ToString();
-			Debug.Log(score_Count);
-			Destroy(other.gameObject);
-		}
+public class AddCoin : MonoBehaviour
+{
+    // Use this for initialization
+    public Text score_Coin;
+    private int score_Count = 0;
+    private GameObject coin;
+    void Start()
+    {
+        score_Coin.text = score_Count.ToString();
+        score_Coin.text = PlayerPrefs.GetInt("Record").ToString();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coin = GameObject.FindGameObjectWithTag("coin");
+        Destroy(coin, 5);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("123");
+        if (other.gameObject.tag == "coin")
+        {
+            score_Count += 1;
+            score_Coin.text = score_Count.ToString();
+            PlayerPrefs.SetInt("Record", score_Count);
+            Debug.Log(score_Count);
+            Destroy(other.gameObject);
+        }
     }
 }
